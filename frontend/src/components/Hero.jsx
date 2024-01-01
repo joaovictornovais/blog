fca415a02da351637177d612f754cd699ccc5e4d
joaovictor <joao.dipaay@gmail.com>
2024-01-01 20:10:56 -0300

    feat: load api news

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,11 +1,31 @@
+import { useState } from "react";
 import TabPost from "./TabPost"
+import axios from "axios";
+import { useEffect } from "react";
 
 const Hero = () => {
+
+  const [posts, setPosts] = useState([]);
+
+  const loadPosts = () => {
+    axios.get('http://localhost:8080/posts').then(data => (
+      setPosts(data.data)
+    ))
+  }
+
+  useEffect(() => {
+    loadPosts();
+  }, [])
+
   return (
     <main className="h-screen bg-neutral-800 text-zinc-100 pt-[48px]">
         <div className="max-w-6xl mx-auto py-6 px-4">
             <div className="grid grid-cols-1 gap-4">
-                <TabPost title="Hello, world!" subtitle="This is my first post!" date="01/01/2023"/>
+                {
+                  posts.map((post) => (
+                    <TabPost key={post.id} title={post.title} subtitle={post.subtitle} date={post.date}/>
+                  ))
+                }
             </div>
         </div>
     </main>
